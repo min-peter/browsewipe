c1a94dab0de50647d5573d6e82628a357bcfc62f
min-peter <m.primedev@gmail.com>
2025-12-29 19:58:49 +0700

    fix: update fix for route base url

--- web/app/api/auth/[...nextauth]/route.ts
+++ web/app/api/auth/[...nextauth]/route.ts
@@ -63,11 +63,6 @@ export const authOptions: NextAuthOptions = {
       }
       return session;
     },
-    async redirect({ url, baseUrl }) {
-      if (url.startsWith("/")) return `${baseUrl}${url}`;
-      else if (new URL(url).origin === baseUrl) return url;
-      return baseUrl;
-    },
   },
 
   useSecureCookies: process.env.NODE_ENV === "production",
